a1a84659d9fd286f272041ba2f7c74e96769d91a
Alice-Rossi-03 <alicerossi0310@gmail.com>
2024-01-22 18:48:59 +0100

    feat (js) /for-cycle-not-functional

---
+++ assets/script/main.js
@@ -0,0 +1,87 @@
+// CONSEGNA -> Creazione di un carosello 
+
+// Dato un array di oggetti letterali con:
+    // - url dell’immagine
+    // - titolo
+    // - descrizione
+
+
+// Primo Step -> Come nel primo carosello realizzato, focalizziamoci prima sulla creazione del markup statico: costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider.
+
+// Secondo Step -> Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+// Al click dell'utente sulle frecce verso sinistra o destra, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+
+// Terzo Step -> Aggiungere il "ciclo infinito" del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
+
+
+// Creazione Array Oggetti 
+
+
+let songArray = [
+    {
+        img: "./assets/img/bring-me-the-horizon.jpg",
+        title: "Can You Feel My Heart?", 
+        author: "By: Bring Me The Horizon"
+    },
+    {
+        img: "./assets/img/the-dresden-dolls.jpg",
+        title: "My Alcoholic Friends", 
+        author: "By: The Dresden Dolls"
+    },
+    {
+        img: "./assets/img/the-neighbourhood.jpeg",
+        title: "Softcore", 
+        author: "By: The Neighbourhood"
+    },
+    {
+        img: "./assets/img/mother-mother.jpg",
+        title: "Hayloft II", 
+        author: "By: Mother Mother"
+    },
+    {
+        img: "./assets/img/my-chemical-romance.jpg",
+        title: "Teenagers", 
+        author: "By: My Chemical Romance"
+    },
+]
+
+let mainHtml = document.querySelector("main")
+
+let topImgContainer = document.getElementById("top-img-container")
+
+let leftArrow = document.getElementById("leftArrow")
+let rightArrow = document.getElementById("rightArrow")
+
+leftArrow.addEventListener("click", function(){
+    // console.log("left-click")
+
+    for(let i = 0; i < songArray.length; i++){
+        topImgContainer.innerHTML += `
+            <figure>
+                <img src="${songArray[i].img}" alt="">
+                <div class="description">
+                    <div class="bigger-text">${songArray[i].title}</div>
+                    <div class="smaller-text">${songArray[i].author}</div>
+                </div>
+            </figure>
+        `
+    }
+
+})
+
+rightArrow.addEventListener("click", function(){
+    // console.log("right-click")
+
+    for(let i = songArray.length; i > 0 ; i--){
+        topImgContainer.innerHTML += `
+            <figure>
+                <img src="${songArray[i].img}" alt="">
+                <div class="description">
+                    <div class="bigger-text">${songArray[i].title}</div>
+                    <div class="smaller-text">${songArray[i].author}</div>
+                </div>
+            </figure>
+        `
+    }
+})
+
